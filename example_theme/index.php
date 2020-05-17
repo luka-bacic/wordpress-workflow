5a105bade051660174650bd5753e4cb190343d2f
Luka Bacic <luka.bacic278@gmail.com>
2020-05-17 13:14:16 +1000

    delete old images, add 1 in index.php

--- example_theme/index.php
+++ example_theme/index.php
@@ -11,7 +11,17 @@
       <h2><?php the_title(); ?></h2>
     <?php endwhile; ?>
   <?php else : ?>
-    <h2>No posts found.</h2>
+    <h2>No posts found, but 1 rad lizard was found:</h2>
   <?php endif; ?>
+
+  <figure>
+    <img
+      src="<?php bloginfo( 'template_directory' ) . '/dist/images/lizard.jpg'; ?>"
+      alt="pink lizard chilling"
+    />
+    <figcaption>
+      Photo by <a href="https://unsplash.com/@magdalenakulamanchee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Magdalena Kula Manchee</a> on <a href="https://unsplash.com/">Unsplash</a>
+    </figcaption>
+  </figure>
 </body>
 </html>
