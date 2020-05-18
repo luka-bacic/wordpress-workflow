3c705e55fea8bdf72d4e43fe87003ececdda3ce3
Luka Bacic <luka.bacic278@gmail.com>
2020-05-18 13:33:28 +1000

    update reload rules; update index.php to include scripts

--- example_theme/index.php
+++ example_theme/index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <?php wp_head(); ?>
 </head>
 <body>
   <?php if ( have_posts() ) : ?>
@@ -11,17 +12,19 @@
       <h2><?php the_title(); ?></h2>
     <?php endwhile; ?>
   <?php else : ?>
-    <h2>No posts found, but 1 rad lizard was found:</h2>
+    <h2>No posts found.</h2>
   <?php endif; ?>
 
-  <figure>
+  <figure> 
+    <!-- You include images by referencing them from the `dist` directory, like this: -->
     <img
-      src="<?php bloginfo( 'template_directory' ) . '/dist/images/lizard.jpg'; ?>"
+      src="<?php echo bloginfo( 'template_directory' ) . '/dist/images/lizard.jpg'; ?>"
       alt="pink lizard chilling"
     />
     <figcaption>
       Photo by <a href="https://unsplash.com/@magdalenakulamanchee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Magdalena Kula Manchee</a> on <a href="https://unsplash.com/">Unsplash</a>
     </figcaption>
   </figure>
+  <?php wp_footer(); ?>
 </body>
 </html>
